2b82c2cf9aadea3d1e4c12cdd1d8b91fd39bd4c6
saidpar <thebigrokh@gmail.com>
2017-10-04 20:30:58 -0700

    implement getResponse method

--- AnalyticSprayDown/app/src/main/java/com/sprayme/teamrsm/analyticspraydown/utilities/NetworkUtils.java
+++ AnalyticSprayDown/app/src/main/java/com/sprayme/teamrsm/analyticspraydown/utilities/NetworkUtils.java
@@ -2,8 +2,12 @@ package com.sprayme.teamrsm.analyticspraydown.utilities;
 
 import android.net.Uri;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
  * Utilities to communicate with the network.
@@ -78,4 +82,27 @@ public class NetworkUtils {
         return routesUrl;
     }
 
+    /*
+    * Returns the entire result from the HTTP response.
+    * */
+    public String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            }
+            else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
