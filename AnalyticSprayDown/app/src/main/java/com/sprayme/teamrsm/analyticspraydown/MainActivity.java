7c0d24f4ec4a399e775adfe756d6b79c2c06a3a8
saidpar <thebigrokh@gmail.com>
2017-10-04 23:13:06 -0700

    menu item build pyramid

--- AnalyticSprayDown/app/src/main/java/com/sprayme/teamrsm/analyticspraydown/MainActivity.java
+++ AnalyticSprayDown/app/src/main/java/com/sprayme/teamrsm/analyticspraydown/MainActivity.java
@@ -2,6 +2,8 @@ package com.sprayme.teamrsm.analyticspraydown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.sprayme.teamrsm.analyticspraydown.utilities.MPQueryTask;
 
@@ -20,5 +22,21 @@ public class MainActivity extends AppCompatActivity
     public void processFinish(String output) {
         /* Here we will receive the result fired from the mpQueryTask
         * onPostExecute(result) method. */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.build_pyramid) {
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
