0ee3d2fc6dab9a570e2210523765ea6bacf2bfef
climbak <r.courreges@gmail.com>
2017-10-10 00:24:23 -0700

    small updates to login activity

--- AnalyticSprayDown/app/src/main/java/com/sprayme/teamrsm/analyticspraydown/UserLoginActivity.java
+++ AnalyticSprayDown/app/src/main/java/com/sprayme/teamrsm/analyticspraydown/UserLoginActivity.java
@@ -1,9 +1,11 @@
 package com.sprayme.teamrsm.analyticspraydown;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.sprayme.teamrsm.analyticspraydown.utilities.DataCache;
 
@@ -16,6 +18,13 @@ public class UserLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
         dataCache = DataCache.getInstance();
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getBundleExtra("")..getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
     }
 
     public void onLogin(View view){
